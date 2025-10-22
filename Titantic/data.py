e9a06e2df36e65aa21ee952bec5e5ae4ec3a5864
daniloldn <112765545+daniloldn@users.noreply.github.com>
2025-10-22 16:56:27 +0100

    inital steps

---
+++ Titantic/data.py
@@ -0,0 +1,40 @@
+#Script to load data
+import pandas as pd
+import numpy as np
+
+def missing_data(df):
+
+    total =df.isnull().sum()
+    percent = (df.isnull().sum()/df.isnull().count()*100)
+    tt = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
+    types = []
+    for col in df.columns:
+        dtype = str(df[col].dtype)
+        types.append(dtype)
+    tt['Types'] = types
+    
+    df_missing_train = np.transpose(tt)
+
+    return df_missing_train
+
+def data_frequency(df):
+    total = df.count()
+    tt = pd.DataFrame(total)
+    tt.columns = ['Total']
+    items = []
+    vals = []
+    for col in df.columns:
+        try:
+            itm = df[col].value_counts().index[0]
+            val = df[col].value_counts().values[0]
+            items.append(itm)
+            vals.append(val)
+        except Exception as ex:
+            print(ex)
+            items.append(0)
+            vals.append(0)
+        continue
+    tt['Most frequent item'] = items
+    tt['Frequence'] = vals
+    tt['Percent from total'] = np.round(vals / total * 100, 3)
+    return np.transpose(tt)
